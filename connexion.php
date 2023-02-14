bf599fc198ab8261a2d6cba3cd9475064e419227
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-02-14 18:56:25 +0100

    test

--- connexion.php
+++ connexion.php
@@ -2,7 +2,7 @@
 include 'Classes/Users.php';
 session_start();
 
-if (isset($_POST['loginC'])) {
+/*if (isset($_POST['loginC'])) {
     $login = $_POST['loginC'];
     $password = $_POST['passwordC'];
     $users = new Users();
@@ -15,6 +15,23 @@ if (isset($_POST['loginC'])) {
     } else {
         header('http/1.1 403 Forbidden');
         echo json_encode(['status' => '403', 'message' => 'Login ou mot de passe incorrect']);
+    }
+}*/
+if (isset($_POST['loginC'])) {
+    $login = $_POST['loginC'];
+    $password = $_POST['passwordC'];
+    $users = new Users();
+    $connexion = $users->login($login, $password);
+    if ($connexion) {
+        $_SESSION['login'] = $login;
+        $_SESSION['password'] = $password;
+        $response1 = array('success' => true);
+        echo json_encode($response1);
+        header('Content-type: application/json; charset=utf-8');
+    } else {
+        $response2 = array('success' => false);
+        echo json_encode($response2);
+        header('Content-type: application/json; charset=utf-8');
     }
 }
 var_dump($_SESSION);
--- script/login.js
+++ script/login.js
@@ -1,6 +1,6 @@
 const loginForm = document.querySelector('#LoginForm');
 
-loginForm.addEventListener('submit', (e) => {
+/*loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let DataFormLogin = new FormData(loginForm);
     fetch('connexion.php', {
@@ -24,4 +24,34 @@ loginForm.addEventListener('submit', (e) => {
     .catch((error) => {
         console.error(error);
     });
+});*/
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let DataFormLogin = new FormData(loginForm);
+    fetch('connexion.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(DataFormLogin)
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            if (data.success === true) {
+                // Le compte a bien été créé
+                let msg = document.querySelector("#msgCount");
+                msg.innerHTML = "Vous êtes connecté";
+                msg.classList.add("alert-success");
+                msg.classList.remove("alert-danger");
+            } else {
+                let msg = document.querySelector("#msgCount");
+                msg.innerHTML = "Mauvais identifiants";
+                msg.classList.add("alert-danger");
+                msg.classList.remove("alert-success");
+            }
+        })
 });
+
