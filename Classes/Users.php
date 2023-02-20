16321065832a0ac361ccd63c8be5c084b1bc51a7
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-02-20 10:55:47 +0100

    ajout bdd

--- Classes/Users.php
+++ Classes/Users.php
@@ -4,6 +4,14 @@ define("DB_NAME", 'livreor');
 define('DB_CHARSET', 'utf8');
 define("DB_USER", 'root');
 define("DB_PASSWORD", '');
+
+//plesk
+/*define("DB_HOST", 'localhost');
+define("DB_NAME", 'jules-jean-louis_livreor');
+define('DB_CHARSET', 'utf8');
+define("DB_USER", 'jules');
+define("DB_PASSWORD", '3T9w!ql77');*/
+
 class Users
 {
     protected $db;
